a45d9f3ba104387fc50baf0da6f48c9987e8d22e
The_Legolas <lucalegom@gmail.com>
2025-07-04 00:04:46 +0200

    Added comment's and prepared for the next lesson which I will finnish tommorow

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+#This is where the main thing happens
+def main():
+    text = get_book_text("books/frankenstein.txt")
+
+    # This splits are string up into a list so I can
+    # count up the number of words.
+    words = text.split()
+    
+    # This prints the required assignment
+    print(f"{len(words)} words found in the document")
+
+
+# This function finds the file with a given filepath
+# And then converts it into a string for python to read
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
     
-
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(text)
 
 
 main()
