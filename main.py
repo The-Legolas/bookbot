af1017e8f25f406ffa6a35ebe48729378b1f2903
The_Legolas <lucalegom@gmail.com>
2025-07-05 20:58:03 +0200

    finished CH2 L5

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+
+
 #This is where the main thing happens
 def main():
     text = get_book_text("books/frankenstein.txt") 
@@ -12,13 +15,6 @@ def main():
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
-    
-def get_num_words(text):
-    # This splits the strings up into a list so I can
-    # count up the number of words.
-    words = text.split()
 
-    # This takes the text-list and splits them so I can count them later
-    return len(words)
 
 main()
---
+++ stats.py
@@ -0,0 +1,8 @@
+  
+def get_num_words(text):
+    # This splits the strings up into a list so I can
+    # count up the number of words.
+    words = text.split()
+
+    # This takes the text-list and splits them so I can count them later
+    return len(words)
